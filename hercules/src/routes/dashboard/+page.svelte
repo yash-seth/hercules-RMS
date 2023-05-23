c5432f0509f02a7a6eea2fb3c936201847e8bdca
Yash Seth <yashseth2002@gmail.com>
2023-05-23 11:50:37 +0530

    feat: add routing functionality

--- hercules/src/routes/dashboard/+page.svelte
+++ hercules/src/routes/dashboard/+page.svelte
@@ -1,5 +1,7 @@
 <script>
+    // imports
   import Order from "../../lib/Order.svelte";
+  import { goto } from '$app/navigation'
     $: id = 0
     $: name=""
     let orders = []
@@ -18,40 +20,45 @@
 </script>
 
 <div class="dashboard">
-    <div class="order">
-        <div class="create-order">
-            <h3>Give Order</h3>
-            <textarea bind:value={name}></textarea>
-            <button id="add-order" on:click={addOrder}>Add Order</button>
+    <div class="dashboard-main">
+        <div class="order">
+            <div class="create-order">
+                <h3>Give Order</h3>
+                <textarea bind:value={name}></textarea>
+                <button id="add-order" on:click={addOrder}>Add Order</button>
+            </div>
+        </div>
+        <div class="order-details">
+            <div class="pending">
+                <h3>Pending Orders</h3>
+                {#each orders as order}
+                <Order name={order.name} id={order.id} bind:completedOrder={completedOrder} orders={orders} on:change={handleChange}/>
+                {/each}
+            </div>
+            <div class="completed">
+                <div class="completed-list-controls">
+                    <h3>Completed Orders</h3>
+                    <button on:click={clearCompletedList}>Clear List</button>
+                </div>
+                <ul>
+                    {#each completedOrder as completed_order}
+                    <li><Order name={completed_order.name} id={completed_order.id}/></li>
+                    {/each}
+                </ul>
+            </div>
         </div>
     </div>
-    <div class="order-details">
-        <div class="pending">
-            <h3>Pending Orders</h3>
-            {#each orders as order}
-            <Order name={order.name} id={order.id} bind:completedOrder={completedOrder} orders={orders} on:change={handleChange}/>
-            {/each}
-        </div>
-        <div class="completed">
-            <div class="completed-list-controls">
-                <h3>Completed Orders</h3>
-                <button on:click={clearCompletedList}>Clear List</button>
-            </div>
-            <ul>
-                {#each completedOrder as completed_order}
-                <li><Order name={completed_order.name} id={completed_order.id}/></li>
-                {/each}
-            </ul>
-        </div>
+    <div>
+        <button on:click={() => goto("./")}>Back to Homepage</button>
     </div>
 </div>
 
 <style>
     .dashboard {
         display: flex;
-        flex-direction: row;
+        flex-direction: column;;
         justify-content: center;
-        align-items: flex-start;
+        align-items: center;
     }
 
     .order {
@@ -93,6 +100,13 @@
         display: flex;
         flex-direction: row;
         justify-content: space-between;
+        align-items: center;
+    }
+
+    .dashboard-main {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
         align-items: center;
     }
 </style>
