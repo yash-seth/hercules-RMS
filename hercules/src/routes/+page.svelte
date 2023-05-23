e11bacebd5f20b87ea237a8ef9b16a877a50f713
Yash Seth <yashseth2002@gmail.com>
2023-05-23 11:50:19 +0530

    chore: setup homepage

--- hercules/src/routes/+page.svelte
+++ hercules/src/routes/+page.svelte
@@ -1,1 +1,17 @@
-<h1>Welcome to Hercules Restaurant</h1>
+<script>
+    import { goto } from '$app/navigation'
+</script>
+
+<div class="homepage">
+    <h1>Welcome to Hercules Restaurant</h1>
+    <button on:click={() => goto("./dashboard")}>Login</button>
+</div>
+
+<style>
+    .homepage {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+    }
+</style>
