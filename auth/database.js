6f2b9ae9ccf744d0d2b7726f370920531bab3906
Yash Seth <yashseth2002@gmail.com>
2023-05-27 17:24:14 +0530

    feat(db): add code for user lookup

--- auth/database.js
+++ auth/database.js
@@ -37,11 +37,27 @@ const pool = mysql.createPool({
 
 // delete user from db
 
+// new Promise(async function(resolve, reject) {
+//     const res = await pool.query("DELETE from USERS where username = 'harsh-seth'");
+//     console.log(res);
+//     }).then(
+//         (result) => console.log("Number of records deleted: " + result.affectedRows)
+//     ).catch(
+//         console.log
+//     )
+
+
+// lookup user
+
 new Promise(async function(resolve, reject) {
-    const res = await pool.query("DELETE from USERS where username = 'harsh-seth'");
-    console.log(res);
-    }).then(
-        (result) => console.log("Number of records deleted: " + result.affectedRows)
-    ).catch(
-        console.log
-    )
+    const rows = await pool.query("SELECT * FROM USERS where username = '123'");
+    if(rows) {
+        resolve(rows);
+    } else {
+        reject("Unable to fetch from database.")
+    }
+}).then(
+    (users) => console.log(users)
+).catch(
+    console.log
+)
